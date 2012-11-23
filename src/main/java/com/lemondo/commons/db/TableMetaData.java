b2aac21860de044ce37fded91c016dbeec941267
Giorgi Kandelaki <kandelakig@gmail.com>
2012-11-23 17:56:57 +0400

    Started implementation of TableMetaData

--- src/main/java/com/lemondo/commons/db/TableMetaData.java
+++ src/main/java/com/lemondo/commons/db/TableMetaData.java
@@ -1,6 +1,8 @@
 package com.lemondo.commons.db;
 
 import java.sql.CallableStatement;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -17,9 +19,99 @@ public class TableMetaData implements ModelMetaData<Map<String, Object>> {
 		this.deactivatedFlag = deactivatedFlag;
 	}
 
-	private String genSelectSql(Map<String, Object> options, boolean allRows) {
-		// TODO Auto-generated method stub
-		return null;
+	private class FilterCondition {
+		String columnName;
+		String operator;
+		Object value;
+		int type;
+
+		FilterCondition(String columnName, String operator, Object value, int type) {
+			this.columnName = columnName;
+			this.operator = operator;
+			this.value = value;
+			this.type = type;
+		}
+	}
+
+	private Set<FilterCondition> extractFilter(Map<String, Object> options) {
+		Object filterRaw = options.get("filter");
+		if (filterRaw == null) {
+			return null;
+		} else if (!(filterRaw instanceof Map)) {
+			throw new RuntimeException("BOOM!");
+		} else {
+			Set<FilterCondition> filter = new HashSet<TableMetaData.FilterCondition>();
+
+			@SuppressWarnings("unchecked")
+			Map<String, Object> filterMap = (Map<String, Object>) filterRaw;
+
+			Set<String> filterFields = filterMap.keySet();
+			for (String field : filterFields) {
+				if (columnDef.containsKey(field)) {
+					filter.add(new FilterCondition(field, "=", filterMap.get(field), columnDef.get(field)));
+				}
+			}
+
+			return filter;
+		}
+	}
+
+	private String genFilterString(Set<FilterCondition> filterColumns) {
+		StringBuilder result = new StringBuilder();
+
+		String prefix = "";
+		for (FilterCondition condition : filterColumns) {
+			result.append(prefix).append("`").append(condition.columnName).append(condition.operator).append("?");
+			prefix = " AND ";
+		}
+
+		return result.toString();
+	}
+
+	private String genOrderByString(List<String> sortFields) {
+		if (sortFields == null) {
+			throw new RuntimeException("BOOM!");
+		} else {
+			StringBuilder result = new StringBuilder();
+			
+			String prefix = " ORDER BY ";
+			for (int i = 0; i < sortFields.size(); i++) {
+				result.append(prefix).append("`").append(sortFields.get(i)).append("`");
+				prefix = ",";
+			}
+			
+			return result.toString();
+		}
+	}
+
+	private String genSelectSql(Map<String, Object> options, boolean allRows, Set<FilterCondition> filter, List<String> sortFields) {
+		StringBuilder selectSql = new StringBuilder("SELECT `id`");
+
+		Set<String> columns = columnDef.keySet();
+		for (String column : columns) {
+			selectSql.append(",`").append(column).append("`");
+		}
+
+		selectSql.append(" FROM ").append(tableName);
+
+		String filterPrefix = " WHERE ";
+		if (deactivatedFlag) {
+			selectSql.append(filterPrefix).append("`deactivated` = 0");
+			filterPrefix = " AND ";
+		}
+		if (!allRows) {
+			selectSql.append(filterPrefix).append("`id` = ?");
+			filterPrefix = " AND ";
+		}
+		if (filter != null) {
+			selectSql.append(filterPrefix).append(genFilterString(filter));
+		}
+		if (allRows && sortFields != null) {
+			selectSql.append(genOrderByString(sortFields));
+		}
+		// TODO Add paging here
+
+		return selectSql.toString();
 	}
 
 	private String genInsertSql(Set<String> columns) {
