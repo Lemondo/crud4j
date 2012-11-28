23a776c39cfbc6d13935d99b35aa1b3b6390cba3
Giorgi Kandelaki <kandelakig@gmail.com>
2012-11-28 16:36:53 +0400

    Made `Helper` `abstract` and added two implementations of it. One will use `DataSource` to obtain connections, and another will just do `DriverManager.getConnection`

--- src/main/java/com/lemondo/commons/db/Helper.java
+++ src/main/java/com/lemondo/commons/db/Helper.java
@@ -2,28 +2,90 @@ package com.lemondo.commons.db;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
-public class Helper {
+public abstract class Helper {
 
-	private final DataSource ds;
-
-	private Connection conn;
 	private List<CallableStatement> statementPool;
 
-	public Helper(DataSource ds) {
-		this.ds = ds;
+	protected Connection conn;
+
+	protected Helper() {
 		this.statementPool = new ArrayList<CallableStatement>();
 	}
 
-	public Connection getConnection() throws SQLException {
-		return (this.conn == null) ? (this.conn = ds.getConnection()) : this.conn;
+	private static class PlainJdbcHelper extends Helper {
+
+		private final String uri;
+		private final String userName;
+		private final String password;
+
+		private PlainJdbcHelper(String driverName, String uri, String userName, String password) {
+			super();
+
+			this.uri = uri;
+			this.userName = userName;
+			this.password = password;
+
+			try {
+				Class.forName(driverName);
+			} catch (ClassNotFoundException e) {
+				throw new RuntimeException("BOOM!", e);
+			}
+		}
+
+		@Override
+		public Connection getConnection() throws SQLException {
+			return (this.conn == null) ? (this.conn = DriverManager.getConnection(uri, userName, password)) : this.conn;
+		}
+
 	}
+
+	private static class DataSourceHelper extends Helper {
+
+		private final DataSource ds;
+
+		private DataSourceHelper(DataSource ds) {
+			super();
+			this.ds = ds;
+		}
+
+		private DataSourceHelper(String dataSourceJndi) {
+			super();
+			try {
+				this.ds = (DataSource) new InitialContext().lookup(dataSourceJndi);
+			} catch (NamingException e) {
+				throw new RuntimeException("BOOM!", e);
+			}
+		}
+
+		@Override
+		public Connection getConnection() throws SQLException {
+			return (this.conn == null) ? (this.conn = ds.getConnection()) : this.conn;
+		}
+	}
+
+	public static Helper getInstance(DataSource ds) {
+		return new DataSourceHelper(ds);
+	}
+
+	public static Helper getInstance(String dataSourceJndi) {
+		return new DataSourceHelper(dataSourceJndi);
+	}
+
+	public static Helper getInstance(String driverName, String uri, String userName, String password) {
+		return new PlainJdbcHelper(driverName, uri, userName, password);
+	}
+
+	public abstract Connection getConnection() throws SQLException;
 
 	public CallableStatement prepareCall(String sqlStatement) throws SQLException {
 		CallableStatement stmnt = this.getConnection().prepareCall(sqlStatement);
