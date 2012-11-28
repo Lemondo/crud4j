66f63e40c251fcfe208d66a2d9b5eb25585dec35
Giorgi Kandelaki <kandelakig@gmail.com>
2012-11-28 17:06:34 +0400

    Added tests for `HashModel.delete` and `hashModel.read`

--- src/test/java/com/lemondo/commons/db/HashModelTest.java
+++ src/test/java/com/lemondo/commons/db/HashModelTest.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.Types;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import org.dbunit.Assertion;
@@ -29,6 +30,7 @@ public class HashModelTest extends DatabaseTestCase {
 
 	private final Helper helper;
 	private final TableMetaData testTable01MetaData;
+	private final TableMetaData testTable02MetaData;
 
 	public HashModelTest() throws FileNotFoundException, IOException, ClassNotFoundException {
 		Properties prop = new Properties();
@@ -51,6 +53,7 @@ public class HashModelTest extends DatabaseTestCase {
 		columnDef.put("password", Types.VARCHAR);
 		columnDef.put("loginenabled", Types.VARCHAR);
 		this.testTable01MetaData = new TableMetaData("test_table", columnDef, true);
+		this.testTable02MetaData = new TableMetaData("test_table", columnDef, false);
 	}
 
 	@Override
@@ -77,14 +80,12 @@ public class HashModelTest extends DatabaseTestCase {
 
 		IDataSet actual = getConnection().createDataSet(new String[] { "test_table" });
 		IDataSet expected = new FlatXmlDataSet(new FileInputStream("src/test/data/out-testCreate.xml"));
-
 		Assertion.assertEquals(expected, actual);
 
 		try {
 			m.create("E011", body);
-			fail("Showld throw an exception when trying to insert duplicate entry");
+			fail("Should throw an exception when trying to insert duplicate entry");
 		} catch (Exception e) {
-			assertTrue(e instanceof Exception);
 		}
 	}
 
@@ -99,26 +100,59 @@ public class HashModelTest extends DatabaseTestCase {
 
 		IDataSet actual = getConnection().createDataSet(new String[] { "test_table" });
 		IDataSet expected = new FlatXmlDataSet(new FileInputStream("src/test/data/out-testUpdate.xml"));
-
 		Assertion.assertEquals(expected, actual);
 
 		assertEquals(0, m.update("E011", body));
 	}
 
-//	public void testDelete() throws Exception {
-//		fail("Not yet implemented");
-//	}
-//
-//	public void testRead() throws Exception {
-//		fail("Not yet implemented");
-//	}
-//
-//	public void testListMapOfStringObject() throws Exception {
-//		fail("Not yet implemented");
-//	}
-//
-//	public void testListOutputStreamMapOfStringObject() throws Exception {
-//		fail("Not yet implemented");
-//	}
+	public void testDeleteWithDeactivatedFlag() throws Exception {
+		HashModel m = new HashModel(testTable01MetaData, helper);
+
+		assertEquals(1, m.delete("E999"));
+
+		IDataSet actual = getConnection().createDataSet(new String[] { "test_table" });
+		IDataSet expected = new FlatXmlDataSet(new FileInputStream("src/test/data/out-testDeleteWithDeactivatedFlag.xml"));
+		Assertion.assertEquals(expected, actual);
+
+		assertEquals(0, m.delete("E011"));
+	}
+
+	public void testDeleteWithoutDeactivatedFlag() throws Exception {
+		HashModel m = new HashModel(testTable02MetaData, helper);
+
+		assertEquals(1, m.delete("E999"));
+
+		IDataSet actual = getConnection().createDataSet(new String[] { "test_table" });
+		IDataSet expected = new FlatXmlDataSet(new FileInputStream("src/test/data/out-testDeleteWithoutDeactivatedFlag.xml"));
+		Assertion.assertEquals(expected, actual);
+	}
+
+	public void testRead() throws Exception {
+		HashModel m = new HashModel(testTable01MetaData, helper);
+
+		Map<String, Object> result = m.read("E001");
+
+		assertEquals("E001", (String) result.get("id"));
+		assertEquals(new Integer(1), (Integer) result.get("empcode"));
+		assertEquals("foo", (String) result.get("loginname"));
+		assertEquals("bar", (String) result.get("password"));
+		assertEquals("y", (String) result.get("loginenabled"));
+
+		try {
+			m.read("D001");
+			fail("Should throw an exception when trying to get deleted entry");
+			m.read("E011");
+			fail("Should throw an exception when trying to get non existent entry");
+		} catch (Exception e) {
+		}
+	}
+
+	// public void testListMapOfStringObject() throws Exception {
+	// fail("Not yet implemented");
+	// }
+	//
+	// public void testListOutputStreamMapOfStringObject() throws Exception {
+	// fail("Not yet implemented");
+	// }
 
 }
