ac1bc34a17569d360543faa409abbdadd8e26f53
Giorgi Kandelaki <kandelakig@gmail.com>
2012-11-29 13:30:18 +0400

    Implemented `JSONModel` (not tested)

---
+++ src/main/java/com/lemondo/commons/db/JSONModel.java
@@ -0,0 +1,93 @@
+package com.lemondo.commons.db;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONWriter;
+
+public class JSONModel extends TableModel<JSONObject, JSONArray> {
+
+	public JSONModel(TableMetaData meta, Helper helper) {
+		super(meta, helper);
+	}
+
+	@Override
+	protected Map<String, Object> bodyAsMap(JSONObject body) {
+		Map<String, Object> result = new HashMap<String, Object>();
+
+		try {
+			@SuppressWarnings("unchecked")
+			Iterator<String> keys = body.keys();
+			while (keys.hasNext()) {
+				String key = keys.next();
+				Object val = body.get(key);
+				if (val instanceof JSONObject) {
+					result.put(key, bodyAsMap((JSONObject) val));
+				} else {
+					result.put(key, val);
+				}
+			}
+		} catch (JSONException e) {
+			throw new RuntimeException("BOOM!", e);
+		}
+
+		return result;
+	}
+
+	@Override
+	protected JSONObject readRow(ResultSet rs, ResultSetMetaData rsmd, int numColumns) throws SQLException {
+		try {
+			JSONObject result = new JSONObject();
+			for (int i = 1; i <= numColumns; i++) {
+				result.append(rsmd.getColumnName(i), rs.getObject(i));
+			}
+			return result;
+		} catch (JSONException e) {
+			throw new RuntimeException("BOOM!", e);
+		}
+	}
+
+	@Override
+	protected JSONArray readAll(ResultSet rs, ResultSetMetaData rsmd, int numColumns) throws SQLException {
+		JSONArray result = new JSONArray();
+		while (rs.next()) {
+			result.put(readRow(rs, rsmd, numColumns));
+		}
+		return result;
+	}
+
+	@Override
+	protected void writeRows(OutputStream out, ResultSet rs, ResultSetMetaData rsmd, int numColumns) throws SQLException {
+		OutputStreamWriter ow = new OutputStreamWriter(out);
+		JSONWriter jw = new JSONWriter(ow);
+
+		try {
+			jw.array();
+			while (rs.next()) {
+				jw.object();
+				for (int i = 1; i <= numColumns; i++) {
+					jw.key(rsmd.getColumnName(i)).value(rs.getObject(i));
+				}
+				jw.endObject();
+				ow.flush();
+			}
+			jw.endArray();
+		} catch (JSONException e) {
+			throw new RuntimeException("BOOM!", e);
+		} catch (IOException e) {
+			throw new RuntimeException("BOOM!", e);
+		}
+
+	}
+
+}
